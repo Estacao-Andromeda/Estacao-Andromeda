18a6834ac6a4d32f50fd42d44b44a09eb7130f4d
MaiaArai <158123176+YaraaraY@users.noreply.github.com>
2025-12-28 06:30:45 -0300

    Cosmetic Job Titles (#2407)
    Co-authored-by: funkystationbot <funky@funkystation.org>
    Co-authored-by: taydeo <td12233a@gmail.com>

--- Content.Server.Database/Migrations/Postgres/20251225053605_DBJobAlternateTitle.Designer.cs
+++ Content.Server.Database/Migrations/Postgres/20251225053605_DBJobAlternateTitle.Designer.cs
@@ -43,6 +43,7 @@ using System.Text.Json;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -52,9 +53,11 @@ using NpgsqlTypes;
 namespace Content.Server.Database.Migrations.Postgres
 {
     [DbContext(typeof(PostgresServerDbContext))]
-    partial class PostgresServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251225053605_DBJobAlternateTitle")]
+    partial class DBJobAlternateTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -951,11 +954,6 @@ namespace Content.Server.Database.Migrations.Postgres
                         .IsRequired()
                         .HasColumnType("text")
                         .HasColumnName("borg_name");
-                        
-                    b.Property<string>("BarkVoice")
-                        .IsRequired()
-                        .HasColumnType("text")
-                        .HasColumnName("bark_voice");
 
                     b.Property<string>("CharacterName")
                         .IsRequired()
